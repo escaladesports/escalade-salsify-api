997181857730c69bf0df2210dae9b738c49302ec
Taylor <tbaustin1992@gmail.com>
2018-05-07 17:00:06 -0500

    turning xlsx into json almost done, api still not working

--- dev/dev.js
+++ dev/dev.js
@@ -1,3 +1,27 @@
-import Boilerplate from '../src/index'
-const obj = Boilerplate()
-console.log(obj)
+import config from '../config.json';
+import path from 'path';
+import XLSX from 'xlsx';
+
+const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
+const sheet_name_list = workbook.SheetNames;
+sheet_name_list.forEach(function(y) {
+  console.log(XLSX.utils.sheet_to_json(workbook.Sheets[y]));
+});
+
+// const options = {
+//   url:
+//     'https://app.salsify.com/api/orgs/s-9c2a072b-2f59-495e-b089-121deba82448/export_runs/',
+//   headers: {
+//     Authorization:
+//       'Bearer f2653e0a682e8f524222533233bad7c54a0f5377d3920ddd302353bdb3652f42',
+//     'Content-Type': 'application/json'
+//   }
+// };
+// fetch(options.url, {
+//   method: 'POST',
+//   headers: options.headers,
+//   body: JSON.stringify(config)
+// })
+//   .then(res => res.json())
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
