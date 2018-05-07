2898dbe7da2e496fdda3b2d1a9cfb64ceaa8bb8b
tbaustin <tbaustin1992@gmail.com>
2018-05-07 18:40:59 -0500

    test json formate on prod, api still not working

--- dev/dev.js
+++ dev/dev.js
@@ -1,11 +1,13 @@
 import config from '../config.json';
 import path from 'path';
 import XLSX from 'xlsx';
+import fs from 'fs-extra';
 
 const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
 const sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) {
-  console.log(XLSX.utils.sheet_to_json(workbook.Sheets[y]));
+sheet_name_list.forEach(async y => {
+  const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+  await fs.outputJson(path.resolve(__dirname, `../dist/JSON/${y}.json`), sheet);
 });
 
 // const options = {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
+import config from '../config.json';
+import path from 'path';
+import XLSX from 'xlsx';
+import fs from 'fs-extra';
 
-console.log('Module loaded')
-
-export default function(){
-	const def = {
-		test: '123'
-	}
-	const obj = {
-		anotherTest: 'abc',
-		...def,
-	}
-	return obj
-}
+const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
+const sheet_name_list = workbook.SheetNames;
+sheet_name_list.forEach(async y => {
+  const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+  await fs.outputJson(path.resolve(__dirname, `../dist/JSON/${y}.json`), sheet);
+});
