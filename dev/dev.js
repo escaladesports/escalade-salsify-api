63b62ef7b6d2f27206f228d92efb5c75308ad735
Taylor <tbaustin1992@gmail.com>
2018-05-08 08:03:48 -0500

    add test file

--- dev/dev.js
+++ dev/dev.js
@@ -3,21 +3,24 @@ import path from 'path';
 import XLSX from 'xlsx';
 import fs from 'fs-extra';
 
-const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
-const sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(async y => {
+const itemsToJSON = async () => {
   await fs.outputJson(path.resolve(__dirname, '../dist/JSON/test.json'), {
     hello: 'World'
   });
-  const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
-  sheet.map(async item => {
-    await fs.outputJson(
-      path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
-      item
-    );
-  });
-});
 
+  // const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
+  // const sheet_name_list = workbook.SheetNames;
+  // sheet_name_list.forEach(y => {
+  //   const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+  //   sheet.map(async item => {
+  //     await fs.outputJson(
+  //       path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
+  //       item
+  //     );
+  //   });
+  // });
+};
+itemsToJSON();
 // const options = {
 //   url:
 //     'https://app.salsify.com/api/orgs/s-9c2a072b-2f59-495e-b089-121deba82448/export_runs/',
--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import path from 'path';
 import XLSX from 'xlsx';
 import fs from 'fs-extra';
 
-const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
-const sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(y => {
-  const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
-  sheet.map(async item => {
-    await fs.outputJson(
-      path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
-      item
-    );
+const itemsToJSON = async () => {
+  await fs.outputJson(path.resolve(__dirname, '../dist/JSON/test.json'), {
+    hello: 'World'
   });
-});
+
+  const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
+  const sheet_name_list = workbook.SheetNames;
+  sheet_name_list.forEach(y => {
+    const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+    sheet.map(async item => {
+      await fs.outputJson(
+        path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
+        item
+      );
+    });
+  });
+};
+
+itemsToJSON();
