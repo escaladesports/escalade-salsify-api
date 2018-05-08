33e766aec9f84c8853610b1ae656550895242881
Taylor <tbaustin1992@gmail.com>
2018-05-08 07:41:47 -0500

    add await

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import fs from 'fs-extra';
 
 const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
 const sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(async y => {
+sheet_name_list.forEach(y => {
   console.log(y);
   const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
-  sheet.map(item => {
-    await fs.outputJson(path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`), item);
+  sheet.map(async item => {
+    await fs.outputJson(
+      path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
+      item
+    );
   });
-  
 });
