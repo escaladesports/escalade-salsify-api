a4c674cab13255b8731658f077499494195e84bd
Taylor <tbaustin1992@gmail.com>
2018-05-08 15:52:07 -0500

    add in first 2 sections

--- dev/dev.js
+++ dev/dev.js
@@ -2,39 +2,34 @@ import config from '../config.json';
 import path from 'path';
 import XLSX from 'xlsx';
 import fs from 'fs-extra';
+import fetch from 'isomorphic-fetch';
+import { connectToDatabase } from '../utils/db';
+import Sheet from '../models/Sheet';
 
-const itemsToJSON = async () => {
-  await fs.outputJson(path.resolve(__dirname, '../dist/JSON/test.json'), {
-    hello: 'World'
-  });
-
-  // const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
-  // const sheet_name_list = workbook.SheetNames;
-  // sheet_name_list.forEach(y => {
-  //   const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
-  //   sheet.map(async item => {
-  //     await fs.outputJson(
-  //       path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
-  //       item
-  //     );
-  //   });
-  // });
+const fetchSheet = async () => {
+  await connectToDatabase();
+  const storedData = await Sheet.find({});
+  if (storedData.length > 0) {
+    const storedSheet = storedData[0];
+    if (storedSheet.status === 'completed' && storedSheet.url !== null) {
+      const xlsxFile = await fetch(storedSheet.url).then(res => res.buffer());
+      sheetToJSON(xlsxFile);
+    }
+  }
 };
-itemsToJSON();
-// const options = {
-//   url:
-//     'https://app.salsify.com/api/orgs/s-9c2a072b-2f59-495e-b089-121deba82448/export_runs/',
-//   headers: {
-//     Authorization:
-//       'Bearer f2653e0a682e8f524222533233bad7c54a0f5377d3920ddd302353bdb3652f42',
-//     'Content-Type': 'application/json'
-//   }
-// };
-// fetch(options.url, {
-//   method: 'POST',
-//   headers: options.headers,
-//   body: JSON.stringify(config)
-// })
-//   .then(res => res.json())
-//   .then(res => console.log(res))
-//   .catch(err => console.log(err));
+
+const sheetToJSON = async xlsxFile => {
+  const workbook = XLSX.read(xlsxFile, { type: 'buffer' });
+  const sheet_name_list = workbook.SheetNames;
+  sheet_name_list.forEach(y => {
+    const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+    sheet.map(async item => {
+      await fs.outputJson(
+        path.resolve(__dirname, `../dist/JSON/${item['Item Number']}.json`),
+        item
+      );
+    });
+  });
+};
+
+fetchSheet();
--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import config from '../config.json';
 import path from 'path';
 import XLSX from 'xlsx';
 import fs from 'fs-extra';
+import fetch from 'isomorphic-fetch';
+import { connectToDatabase } from '../utils/db';
+import Sheet from '../models/Sheet';
 
-const itemsToJSON = async () => {
-  await fs.outputJson(path.resolve(__dirname, '../dist/JSON/test.json'), {
-    hello: 'World'
-  });
+const fetchSheet = async () => {
+  await connectToDatabase();
+  const storedData = await Sheet.find({});
+  if (storedData.length > 0) {
+    const storedSheet = storedData[0];
+    if (storedSheet.status === 'completed' && storedSheet.url !== null) {
+      const xlsxFile = await fetch(storedSheet.url).then(res => res.buffer());
+      sheetToJSON(xlsxFile);
+    }
+  }
+};
 
-  const workbook = XLSX.readFile(path.resolve(__dirname, '../src/export.xlsx'));
+const sheetToJSON = async xlsxFile => {
+  const workbook = XLSX.read(xlsxFile, { type: 'buffer' });
   const sheet_name_list = workbook.SheetNames;
   sheet_name_list.forEach(y => {
     const sheet = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
@@ -21,4 +32,4 @@ const itemsToJSON = async () => {
   });
 };
 
-itemsToJSON();
+fetchSheet();
