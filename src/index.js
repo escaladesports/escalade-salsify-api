ec9b73cf0df916d517cd36828657491ffe5ff87a
Taylor <tbaustin1992@gmail.com>
2018-05-15 11:28:15 -0500

    try new promise function

--- src/index.js
+++ src/index.js
@@ -8,83 +8,95 @@ import axios from 'axios';
 import { connectToDatabase } from '../utils/db';
 import Sheet from '../models/Sheet';
 
-const listPerPage = 250;
-const options = {
-  baseUrl: `https://app.salsify.com/api/orgs/${process.env.SALSIFY_ORG_ID}`,
-  url: `https://app.salsify.com/api/orgs/${
-    process.env.SALSIFY_ORG_ID
-  }/lists?per_page=${listPerPage}`,
-  headers: {
-    Authorization: `Bearer ${process.env.SALSIFY_API_KEY}`,
-    'Content-Type': 'application/json'
-  }
-};
 const listToJSON = async () => {
-  let updatedList = [];
-  const res = await fetch(options.url, {
-    method: 'GET',
-    headers: options.headers
-  }).then(res => res.json());
-
-  if (res) {
-    const { lists, meta } = res;
-    updatedList = updatedList.concat(lists);
-    const { total_entries, per_page, current_page } = meta;
-    const pages = Math.ceil(total_entries / per_page);
-    let i;
-    if (pages > 1) {
-      for (i = current_page + 1; i < pages; i++) {
-        const response = await fetch(`${options.url}&page=${i}`, {
-          method: 'GET',
-          headers: options.headers
-        }).then(response => response.json());
-        if (response) {
-          updatedList = updatedList.concat(response.lists);
-        }
-      }
+  const listPerPage = 250;
+  const options = {
+    baseUrl: `https://app.salsify.com/api/orgs/${process.env.SALSIFY_ORG_ID}`,
+    url: `https://app.salsify.com/api/orgs/${
+      process.env.SALSIFY_ORG_ID
+    }/lists?per_page=${listPerPage}`,
+    headers: {
+      Authorization: `Bearer ${process.env.SALSIFY_API_KEY}`,
+      'Content-Type': 'application/json'
     }
-  }
-  if (updatedList.length > 0) {
-    updatedList.forEach(async (item, index) => {
-      let updatedProducts = [];
-      const name = item.name
-        .replace(/^\s+|[^\s\w]+|\s+$/g, '')
-        .replace(/\s+/g, '-')
-        .toLowerCase();
-      const products = await fetch(
-        `${options.baseUrl}/products?filter==list:${item.id}&per_page=250`,
-        {
-          method: 'GET',
-          headers: options.headers
-        }
-      ).then(res => res.json());
+  };
 
-      updatedProducts = updatedProducts.concat(products.products);
-      const productPages = Math.ceil(
-        products.meta.total_entries / products.meta.per_page
-      );
+  return new Promise(async (resolve, reject) => {
+    let updatedList = [];
 
-      if (productPages > 1) {
-        for (let i = products.meta.current_page + 1; i < productPages; i++) {
+    const res = await fetch(options.url, {
+      method: 'GET',
+      headers: options.headers
+    })
+      .then(res => res.json())
+      .catch(err => reject(err));
+
+    if (res) {
+      const { lists, meta } = res;
+      updatedList = updatedList.concat(lists);
+      const { total_entries, per_page, current_page } = meta;
+      const pages = Math.ceil(total_entries / per_page);
+      let i;
+      if (pages > 1) {
+        for (i = current_page + 1; i < pages; i++) {
           const response = await fetch(`${options.url}&page=${i}`, {
             method: 'GET',
             headers: options.headers
-          }).then(response => response.json());
+          })
+            .then(response => response.json())
+            .catch(err => reject(err));
           if (response) {
-            updatedProducts = updatedProducts.concat(response.products);
+            updatedList = updatedList.concat(response.lists);
           }
         }
       }
+    }
+    if (updatedList.length > 0) {
+      updatedList.forEach(async (item, index) => {
+        let updatedProducts = [];
+        const name = item.name
+          .replace(/^\s+|[^\s\w]+|\s+$/g, '')
+          .replace(/\s+/g, '-')
+          .toLowerCase();
+        const products = await fetch(
+          `${options.baseUrl}/products?filter==list:${item.id}&per_page=250`,
+          {
+            method: 'GET',
+            headers: options.headers
+          }
+        )
+          .then(res => res.json())
+          .catch(err => reject(err));
 
-      await fs.outputJson(
-        path.resolve(__dirname, `../dist/JSON/lists/${name}.json`),
-        updatedProducts
-      );
-      if (index + 1 === updatedList.length) {
-        console.log('LISTS CREATED');
-      }
-    });
-  }
+        updatedProducts = updatedProducts.concat(products.products);
+        const productPages = Math.ceil(
+          products.meta.total_entries / products.meta.per_page
+        );
+
+        if (productPages > 1) {
+          for (let i = products.meta.current_page + 1; i < productPages; i++) {
+            const response = await fetch(`${options.url}&page=${i}`, {
+              method: 'GET',
+              headers: options.headers
+            })
+              .then(response => response.json())
+              .catch(err => reject(err));
+            if (response) {
+              updatedProducts = updatedProducts.concat(response.products);
+            }
+          }
+        }
+
+        await fs.outputJson(
+          path.resolve(__dirname, `../dist/JSON/lists/${name}.json`),
+          updatedProducts
+        );
+        if (index + 1 === updatedList.length) {
+          resolve('success');
+        }
+      });
+    }
+  });
 };
 
 const fetchSheet = async () => {
@@ -141,5 +153,13 @@ const sheetToJSON = async (xlsxFile, storedSheet) => {
   });
 };
 
-listToJSON();
-// fetchSheet();
+const runApi = async () => {
+  const res = await listToJSON();
+  if (res === 'success') {
+    fetchSheet();
+  } else {
+    process.exit(1);
+  }
+};
+
+runApi();
