8114fafca4bdbb4679b235dd6fd95eb5d2a01953
Taylor <tbaustin1992@gmail.com>
2018-05-15 07:55:09 -0500

    finish list

--- dev/dev.js
+++ dev/dev.js
@@ -1,3 +1,4 @@
+require('envdotjs').load();
 import config from '../config.json';
 import path from 'path';
 import XLSX from 'xlsx';
@@ -6,6 +7,18 @@ import fetch from 'isomorphic-fetch';
 import axios from 'axios';
 import { connectToDatabase } from '../utils/db';
 import Sheet from '../models/Sheet';
+
+const listPerPage = 250;
+const options = {
+  baseUrl: `https://app.salsify.com/api/orgs/${process.env.SALSIFY_ORG_ID}`,
+  url: `https://app.salsify.com/api/orgs/${
+    process.env.SALSIFY_ORG_ID
+  }/lists?per_page=${listPerPage}`,
+  headers: {
+    Authorization: `Bearer ${process.env.SALSIFY_API_KEY}`,
+    'Content-Type': 'application/json'
+  }
+};
 
 const fetchSheet = async () => {
   await connectToDatabase();
@@ -41,4 +54,50 @@ const sheetToJSON = async (xlsxFile, storedSheet) => {
   });
 };
 
-fetchSheet();
+const listToJSON = async () => {
+  let updatedList = [];
+  const res = await fetch(options.url, {
+    method: 'GET',
+    headers: options.headers
+  }).then(res => res.json());
+
+  if (res) {
+    const { lists, meta } = res;
+    updatedList = updatedList.concat(lists);
+    const { total_entries, per_page, current_page } = meta;
+    const pages = Math.ceil(total_entries / per_page);
+    let i;
+    if (pages > 1) {
+      for (i = current_page + 1; i < pages; i++) {
+        const response = await fetch(`${options.url}&page=${i}`, {
+          method: 'GET',
+          headers: options.headers
+        }).then(response => response.json());
+        if (response) {
+          updatedList = updatedList.concat(response.lists);
+        }
+      }
+    }
+  }
+  if (updatedList.length > 0) {
+    updatedList.forEach(async item => {
+      const name = item.name
+        .replace(/^\s+|[^\s\w]+|\s+$/g, '')
+        .replace(/\s+/g, '-')
+        .toLowerCase();
+      const products = await fetch(
+        `${options.baseUrl}/products?filter==list:${item.id}`,
+        {
+          method: 'GET',
+          headers: options.headers
+        }
+      ).then(res => res.json());
+      await fs.outputJson(
+        path.resolve(__dirname, `../dist/JSON/lists/${name}.json`),
+        products
+      );
+    });
+  }
+};
+
+listToJSON();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('evndotjs').load();
 import config from '../config.json';
 import path from 'path';
 import XLSX from 'xlsx';
@@ -6,6 +7,77 @@ import fetch from 'isomorphic-fetch';
 import axios from 'axios';
 import { connectToDatabase } from '../utils/db';
 import Sheet from '../models/Sheet';
+
+const listPerPage = 250;
+const options = {
+  baseUrl: `https://app.salsify.com/api/orgs/${process.env.SALSIFY_ORG_ID}`,
+  url: `https://app.salsify.com/api/orgs/${
+    process.env.SALSIFY_ORG_ID
+  }/lists?per_page=${listPerPage}`,
+  headers: {
+    Authorization: `Bearer ${process.env.SALSIFY_API_KEY}`,
+    'Content-Type': 'application/json'
+  }
+};
+const listToJSON = async () => {
+  let updatedList = [];
+  const res = await fetch(options.url, {
+    method: 'GET',
+    headers: options.headers
+  }).then(res => res.json());
+
+  if (res) {
+    const { lists, meta } = res;
+    updatedList = updatedList.concat(lists);
+    const { total_entries, per_page, current_page } = meta;
+    const pages = Math.ceil(total_entries / per_page);
+    let i;
+    if (pages > 1) {
+      for (i = current_page + 1; i < pages; i++) {
+        const response = await fetch(`${options.url}&page=${i}`, {
+          method: 'GET',
+          headers: options.headers
+        }).then(response => response.json());
+        if (response) {
+          updatedList = updatedList.concat(response.lists);
+        }
+      }
+    }
+  }
+  if (updatedList.length > 0) {
+    updatedList.forEach(async item => {
+      const name = item.name
+        .replace(/^\s+|[^\s\w]+|\s+$/g, '')
+        .replace(/\s+/g, '-')
+        .toLowerCase();
+      const products = await fetch(
+        `${options.baseUrl}/products?filter==list:${item.id}`,
+        {
+          method: 'GET',
+          headers: options.headers
+        }
+      ).then(res => res.json());
+      await fs.outputJson(
+        path.resolve(__dirname, `../dist/JSON/lists/${name}.json`),
+        products
+      );
+    });
+  }
+};
+
+  if (updatedList.length > 0) {
+    updatedList.forEach(async item => {
+      const products = await fetch(
+        `${options.baseUrl}/products?filter==list:${item.id}`
+      ).then(res => res.json());
+
+      await fs.outputJson(
+        path.resolve(__dirname, `../dist/JSON/lists/${item.name}.json`),
+        products
+      );
+    });
+  }
+};
 
 const fetchSheet = async () => {
   await connectToDatabase();
@@ -54,4 +126,5 @@ const sheetToJSON = async (xlsxFile, storedSheet) => {
   });
 };
 
+listToJSON();
 fetchSheet();
