0358bab933dd585acb00df03fbf027c8323b9915
Taylor <tbaustin1992@gmail.com>
2018-07-03 08:13:43 -0500

    test new list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-require('envdotjs').load();
-import config from '../config.json';
-import path from 'path';
-import XLSX from 'xlsx';
-import fs from 'fs-extra';
-import fetch from 'isomorphic-fetch';
-import axios from 'axios';
-import { connectToDatabase } from '../utils/db';
-import Sheet from '../models/Sheet';
-import camelCase from 'camelcase';
-import brandList from '../list.json';
+require('envdotjs').load()
+import config from '../config.json'
+import path from 'path'
+import XLSX from 'xlsx'
+import fs from 'fs-extra'
+import fetch from 'isomorphic-fetch'
+import axios from 'axios'
+import { connectToDatabase } from '../utils/db'
+import Sheet from '../models/Sheet'
+import camelCase from 'camelcase'
+import brandList from '../list.json'
 
 const listToJSON = () => {
-  const listPerPage = 250;
+  const listPerPage = 250
   const options = {
     baseUrl: `https://app.salsify.com/api/orgs/${process.env.SALSIFY_ORG_ID}`,
     url: `https://app.salsify.com/api/orgs/${
@@ -21,24 +21,24 @@ const listToJSON = () => {
       Authorization: `Bearer ${process.env.SALSIFY_API_KEY}`,
       'Content-Type': 'application/json'
     }
-  };
+  }
 
   return new Promise(async (resolve, reject) => {
-    let updatedList = [];
+    let updatedList = []
 
     const res = await fetch(options.url, {
       method: 'GET',
       headers: options.headers
     })
       .then(res => res.json())
-      .catch(err => reject(err));
+      .catch(err => reject(err))
 
     if (res) {
-      const { lists, meta } = res;
-      updatedList = updatedList.concat(lists);
-      const { total_entries, per_page, current_page } = meta;
-      const pages = Math.ceil(total_entries / per_page);
-      let i;
+      const { lists, meta } = res
+      updatedList = updatedList.concat(lists)
+      const { total_entries, per_page, current_page } = meta
+      const pages = Math.ceil(total_entries / per_page)
+      let i
       if (pages > 1) {
         for (i = current_page + 1; i < pages; i++) {
           const response = await fetch(`${options.url}&page=${i}`, {
@@ -46,31 +46,32 @@ const listToJSON = () => {
             headers: options.headers
           })
             .then(response => response.json())
-            .catch(err => reject(err));
+            .catch(err => reject(err))
           if (response) {
-            updatedList = updatedList.concat(response.lists);
+            updatedList = updatedList.concat(response.lists)
           }
         }
       }
     }
-    let productList = [];
+    let productList = []
     // grab list that we want from list.json
     if (updatedList.length > 0) {
       const selectedList = updatedList.filter(l => {
         const name = l.name
           .replace(/^\s+|[^\s\w]+|\s+$/g, '')
           .replace(/\s+/g, '-')
-          .toLowerCase();
-        return brandList.includes(name);
-      });
-
+          .toLowerCase()
+        console.log(name)
+        return brandList.includes(name)
+      })
+      console.log(selectedList)
       // Loop through the list and populate products
       selectedList.forEach(async (list, index) => {
-        let updatedProducts = [];
+        let updatedProducts = []
         const name = list.name
           .replace(/^\s+|[^\s\w]+|\s+$/g, '')
           .replace(/\s+/g, '-')
-          .toLowerCase();
+          .toLowerCase()
         const products = await fetch(
           `${options.baseUrl}/products?filter==list:${
             list.id
@@ -82,42 +83,40 @@ const listToJSON = () => {
         )
           .then(res => {
             if (res.status === 200) {
-              return res.json();
+              return res.json()
             }
           })
-          .catch(err => reject(err));
+          .catch(err => reject(err))
 
         products.products.map(product => {
-          let updatedProduct = { ...product };
+          let updatedProduct = { ...product }
           product.properties.forEach(p => {
             if (p.values.length === 0) {
-              return;
+              return
             }
-            let updatedName = camelCase(
-              p.id.replace(/^\s+|[^\s\w]+|\s+$/g, '')
-            );
+            let updatedName = camelCase(p.id.replace(/^\s+|[^\s\w]+|\s+$/g, ''))
             switch (p.data_type) {
               case 'digital_asset':
                 updatedProduct[updatedName] = p.values.map(
                   value => value.large_url
-                );
-                return;
+                )
+                return
               default:
                 p.values.length > 1
                   ? (updatedProduct[`${updatedName}Arr`] = p.values.map(
                       value => value.name || value.id
                     ))
-                  : (updatedProduct[updatedName] = p.values[0].name);
-                return;
+                  : (updatedProduct[updatedName] = p.values[0].name)
+                return
             }
-          });
-          delete updatedProduct['properties'];
-          updatedProducts.push(updatedProduct);
-        });
+          })
+          delete updatedProduct['properties']
+          updatedProducts.push(updatedProduct)
+        })
 
         const productPages = Math.ceil(
           products.meta.total_entries / products.meta.per_page
-        );
+        )
 
         if (productPages > 1) {
           for (let i = products.meta.current_page + 1; i <= productPages; i++) {
@@ -131,75 +130,75 @@ const listToJSON = () => {
               }
             )
               .then(response => response.json())
-              .catch(err => reject(err));
+              .catch(err => reject(err))
             if (response) {
               response.products.map(product => {
-                let updatedProduct = { ...product };
+                let updatedProduct = { ...product }
                 product.properties.forEach(p => {
                   if (p.values.length === 0) {
-                    return;
+                    return
                   }
                   let updatedName = camelCase(
                     p.id.replace(/^\s+|[^\s\w]+|\s+$/g, '')
-                  );
+                  )
 
                   switch (p.data_type) {
                     case 'digital_asset':
                       updatedProduct[updatedName] = p.values.map(
                         value => value.large_url
-                      );
-                      return;
+                      )
+                      return
                     default:
                       p.values.length > 1
                         ? (updatedProduct[`${updatedName}Arr`] = p.values.map(
                             value => value.name || value.id
                           ))
-                        : (updatedProduct[updatedName] = p.values[0].name);
-                      return;
+                        : (updatedProduct[updatedName] = p.values[0].name)
+                      return
                   }
-                });
-                delete updatedProduct['properties'];
-                updatedProducts.push(updatedProduct);
-              });
+                })
+                delete updatedProduct['properties']
+                updatedProducts.push(updatedProduct)
+              })
             }
           }
         }
         if (updatedProducts.length === products.meta.total_entries) {
-          productList.push(updatedProducts);
+          productList.push(updatedProducts)
           const progress = (
             (productList.length / brandList.length) *
             100
-          ).toFixed(2);
-          const string = `${progress} %  -  lists completed`;
-          console.log(`${string}`);
+          ).toFixed(2)
+          const string = `${progress} %  -  lists completed`
+          console.log(`${string}`)
         }
         await fs.outputJson(
           path.resolve(__dirname, `../dist/lists/${name}.json`),
           updatedProducts
-        );
+        )
         if (productList.length === brandList.length) {
-          resolve('success');
+          resolve('success')
         }
-      });
+      })
     }
-  });
-};
+  })
+}
 
 const runApi = async () => {
   const listRes = await listToJSON().catch(err => {
-    console.log(err);
-    process.exit(1);
-  });
+    console.log(err)
+    process.exit(1)
+  })
   if (listRes === 'success') {
-    console.log('LISTS CREATED');
-    process.exit(0);
+    console.log('LISTS CREATED')
+    process.exit(0)
   } else {
-    console.log(listRes);
-    process.exit(1);
+    console.log(listRes)
+    process.exit(1)
   }
-};
+}
 
-runApi();
+runApi()
 
 // const fetchSheet = () => {
 //   return new Promise(async (resolve, reject) => {
